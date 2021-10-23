10533fccbe8e4eb6047f6d3104138d8292ecaf36
keanallen <keanallenn@gmail.com>
2021-10-23 14:12:28 +0800

    create utility dialog

---
+++ lib/app/widgets/dialogs.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class CustomDialog {
+  static void alert({required String title, required String content}) {
+    Get.defaultDialog(title: title, content: Text(content), actions: [
+      TextButton(onPressed: () => Get.close(1), child: const Text('Okay'))
+    ]);
+  }
+}
