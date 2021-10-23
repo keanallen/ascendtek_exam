0b45bcee7ecbc02f8f2749a8e2e9a8f01f22805c
keanallen <keanallenn@gmail.com>
2021-10-23 14:12:20 +0800

    create custom textbox widget

---
+++ lib/app/widgets/custom_textbox.dart
@@ -0,0 +1,36 @@
+import 'package:ascendtek_exam/app/controller/auth_controller.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextbox extends StatelessWidget {
+  const CustomTextbox({
+    Key? key,
+    required this.controller,
+    required this.label,
+    required this.prefixIcon,
+    this.isPassword = false,
+  }) : super(key: key);
+
+  final TextEditingController controller;
+  final String label;
+  final IconData prefixIcon;
+  final bool isPassword;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: TextField(
+          controller: controller,
+          obscureText: isPassword,
+          decoration: InputDecoration(
+              border: const OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+              hintText: label,
+              prefixIcon: Icon(prefixIcon)),
+        ),
+      ),
+    );
+  }
+}
