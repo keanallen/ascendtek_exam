6a29cfe9f582a71ce01c39a26414cd28a43cfaed
keanallen <keanallenn@gmail.com>
2021-10-24 11:14:03 +0800

    new feature: like,seen post

--- lib/app/controller/home_controller.dart
+++ lib/app/controller/home_controller.dart
@@ -4,6 +4,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:ascendtek_exam/app/model/tags_model.dart';
+import 'package:ascendtek_exam/app/views/auth/login_page.dart';
 import 'package:ascendtek_exam/app/widgets/dialogs.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -17,18 +18,29 @@ class HomeController extends GetxController {
   var selectedTags = <TagModel>[].obs;
   var filteredTags = <TagModel>[].obs;
   var imagePath = "".obs;
+  var tempLiked = [].obs;
+  var viewedPost = [].obs;
   XFile? image;
 
   @override
   void onInit() async {
-    user.value = await ParseUser.currentUser();
-    getTags();
     super.onInit();
+    var getUser = await ParseUser.currentUser();
+
+    print("=================================================");
+    print(getUser);
+    print("=================================================");
+    if (getUser == null) {
+      Get.offAll(() => const LoginPage());
+    } else {
+      user.value = getUser;
+      getTags();
+    }
   }
 
-  getNameAvatar() {
+  getNameAvatar({String baseName = ''}) {
     String avatar = "";
-    var name = user.value.get('name');
+    var name = baseName.isEmpty ? user.value.get('name') : baseName;
     if (name.toString().contains(" ")) {
       var part1 = name.toString().split(" ")[0][0];
       var part2 = name.toString().split(" ")[1][0];
@@ -104,6 +116,7 @@ class HomeController extends GetxController {
       ParseObject upload = ParseObject("Uploads");
       upload.set('uploader', me);
       upload.set('image', imgUrl);
+      upload.set('liked', []);
       upload.set(
           'tags', selectedTags.map((element) => element.tagname).toList());
       var res = await upload.save();
@@ -116,5 +129,66 @@ class HomeController extends GetxController {
             content: 'You\'ve successfully uploaded an image!');
       }
     }
+  }
+
+  Future getUploads() async {
+    ParseObject uploads = ParseObject("Uploads");
+    QueryBuilder query = QueryBuilder(uploads);
+    var response = await query.query();
+    if (response.success && response.results != null) {
+      var body = jsonDecode(response.results.toString());
+      print(body);
+    }
+  }
+
+  void likePost(String postId) async {
+    tempLiked.add(postId);
+    var me = await ParseUser.currentUser();
+    ParseObject post = ParseObject("Uploads");
+    post.objectId = postId;
+
+    //post.addRelation('likes', [me]);
+    post.setAddUnique('liked', me!.objectId);
+    var response = await post.save();
+    if (response.success) {
+      print('Liked!');
+      print(response.success);
+    }
+  }
+
+  void unlikePost(String postId) async {
+    tempLiked.removeWhere((element) => element == postId);
+
+    var me = await ParseUser.currentUser();
+    ParseObject post = ParseObject("Uploads");
+    post.objectId = postId;
+
+    //post.addRelation('likes', [me]);
+    post.setRemove('liked', me!.objectId);
+    ;
+    var response = await post.save();
+    if (response.success) {
+      print('UNLiked!');
+      print(response.success);
+    }
+  }
+
+  void viewPost(String postId, String userId, num visibility) async {
+    ParseObject uploads = ParseObject("Uploads");
+    uploads.objectId = postId;
+    print("$postId is $visibility% visible");
+    if (visibility >= 80 && !viewedPost.contains(postId)) {
+      uploads.setAddUnique('viewers', userId);
+      var response = await uploads.save();
+      if (response.success) {
+        viewedPost.add(postId);
+      }
+    }
+  }
+
+  void logout() async {
+    CustomDialog.spiner(label: 'Signing out...');
+    await user.value.logout();
+    Get.offAll(() => const LoginPage());
   }
 }
--- lib/app/views/home/home_page.dart
+++ lib/app/views/home/home_page.dart
@@ -1,8 +1,13 @@
 import 'package:ascendtek_exam/app/controller/home_controller.dart';
 import 'package:ascendtek_exam/app/views/home/upload_page.dart';
+import 'package:ascendtek_exam/app/widgets/added_chip.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:get/get.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,7 +15,14 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var home = Get.put<HomeController>(HomeController());
+
+    ParseObject uploads = ParseObject("Uploads");
+    QueryBuilder query = QueryBuilder(uploads);
+    query.includeObject(["uploader"]);
+    query.orderByDescending('createdAt');
+
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       appBar: AppBar(
         title: const Text('Home'),
       ),
@@ -71,11 +83,190 @@ class HomePage extends StatelessWidget {
                     ),
                   ],
                 ),
-                onTap: () {},
+                onTap: () => home.logout(),
               )
             ],
           ),
         ),
+      ),
+      body: ParseLiveListWidget<ParseObject>(
+        shrinkWrap: true,
+        query: query,
+        childBuilder: (context, snapshot) {
+          if (snapshot.failed) {
+            return const Text("Something went wrong");
+          } else if (snapshot.hasData) {
+            var name = snapshot.loadedData!.get('uploader').get("name");
+            var postId = snapshot.loadedData!.get('objectId');
+            return VisibilityDetector(
+              key: Key(postId),
+              onVisibilityChanged: (info) {
+                var visiblePercentage = info.visibleFraction * 100;
+
+                home.viewPost(
+                    postId, home.user.value.objectId!, visiblePercentage);
+              },
+              child: PostImage(
+                postId: postId,
+                uploader: name,
+                avatar: home.getNameAvatar(baseName: name),
+                imageUrl: snapshot.loadedData!.get('image'),
+                tags: snapshot.loadedData!.get('tags'),
+                likes: snapshot.loadedData!.get('liked') ?? [],
+                viewers: snapshot.loadedData!.get('viewers'),
+              ),
+            );
+          } else {
+            return const ListTile(
+              leading: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+class PostImage extends StatelessWidget {
+  const PostImage({
+    Key? key,
+    this.uploader,
+    this.avatar,
+    required this.imageUrl,
+    this.tags,
+    required this.postId,
+    required this.likes,
+    this.viewers,
+  }) : super(key: key);
+
+  final String? avatar;
+  final String? uploader;
+  final String imageUrl;
+  final String postId;
+  final List? tags;
+  final List likes;
+  final List? viewers;
+
+  @override
+  Widget build(BuildContext context) {
+    var home = Get.find<HomeController>();
+    var myId = home.user.value.objectId;
+
+    bool liked = likes.contains(myId);
+
+    if (viewers != null) {
+      viewers!.removeWhere((element) => element == myId);
+    }
+
+    return Container(
+      width: context.width,
+      margin: const EdgeInsets.only(bottom: 10),
+      decoration: const BoxDecoration(color: Colors.white),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  child: Text(avatar!),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0),
+                  child: Text(uploader!, style: Get.textTheme.subtitle1),
+                ),
+              ],
+            ),
+          ),
+          const Divider(height: 1),
+          SizedBox(child: CachedNetworkImage(imageUrl: imageUrl)),
+
+          /// LIKE
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Material(
+                color: Colors.white,
+                child: SizedBox(
+                  height: 60,
+                  child: Stack(
+                    alignment: Alignment.bottomCenter,
+                    children: [
+                      SizedBox(
+                        height: 60,
+                        child: Obx(() => IconButton(
+                            onPressed: () => home.tempLiked.contains(postId) ||
+                                    liked
+                                ? Get.find<HomeController>().unlikePost(postId)
+                                : Get.find<HomeController>().likePost(postId),
+                            icon: Icon(
+                              home.tempLiked.contains(postId) || liked
+                                  ? Icons.favorite
+                                  : Icons.favorite_outline,
+                              color: home.tempLiked.contains(postId) || liked
+                                  ? Colors.red
+                                  : Colors.black87,
+                            ))),
+                      ),
+                      SizedBox(
+                        child: Text(likes.length > 1
+                            ? likes.length.toString() + " likes"
+                            : likes.isEmpty
+                                ? ""
+                                : likes.length.toString() + " like"),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 8.0),
+                child: Text(
+                  viewers == null ? "" : "Seen by ${viewers!.length} people",
+                  style: Get.textTheme.caption,
+                ),
+              )
+            ],
+          ),
+          Container(
+            width: context.width,
+            padding: const EdgeInsets.all(5),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(top: 2),
+                  child: Text(
+                    tags!.length > 1 ? "Tags:" : "Tag:",
+                    style: Get.textTheme.caption,
+                  ),
+                ),
+                Expanded(
+                  child: Wrap(
+                    children: List.generate(
+                        tags!.length,
+                        (index) => Container(
+                              padding: const EdgeInsets.all(2),
+                              margin: const EdgeInsets.only(
+                                  bottom: 3, left: 2, right: 2),
+                              decoration: BoxDecoration(
+                                color: Colors.blue[50],
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              child: Text(
+                                tags![index],
+                                style: Get.textTheme.caption!
+                                    .copyWith(color: Colors.blue),
+                              ),
+                            )),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
