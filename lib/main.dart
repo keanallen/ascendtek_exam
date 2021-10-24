da4bb565c364c26a68362edccd9e6e50384785c9
keanallen <keanallenn@gmail.com>
2021-10-24 15:42:59 +0800

    remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:ascendtek_exam/app/views/auth/login_page.dart';
-import 'package:ascendtek_exam/app/views/home/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
